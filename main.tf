417d30c1665d50d156b8ff3902c5487b63b7969e
findkim <kngo@hashicorp.com>
2021-02-02 12:55:07 -0600

    Template files and documentation for building Terraform modules for Consul Terraform Sync

---
+++ main.tf
@@ -0,0 +1,53 @@
+terraform {
+  required_providers {
+    # Provider source is used for Terraform discovery and installation of
+    # providers. Declare source for all providers required by the module.
+    myprovider = {
+      source  = "namespace/myprovider"
+      version = "~>1.1.0"
+    }
+  }
+}
+
+#
+# Declare resource blocks to describe module behaviors for the infrastructure.
+#
+# The example block below describes creating an address group for each Consul
+# service and applies an existing policy to the group.
+#
+resource "myprovider_address_group" "consul_service" {
+  for_each = local.consul_services
+
+  name     = "${var.address_group_prefix}${each.key}"
+  tags     = var.address_group_tags
+  policies = [each.value.cts_user_defined_meta["policy_name"]]
+}
+
+#
+# You can utilize the locals block to manipulate the var.services variable
+# into a data structure for your module.
+#
+# The example below converts var.services to a map of service names to a list
+# of service instances.
+locals {
+  # Create a map of service names to instance IDs to then build
+  # a map of service names to instances
+  consul_service_ids = transpose({
+    for id, s in var.services : id => [s.name]
+  })
+
+  # Group service instances by service name
+  # services = {
+  #   "app" = [
+  #     {
+  #       "id" = "app"
+  #       "name" = "app"
+  #       "node_address" = "192.168.10.10"
+  #     }
+  #   ]
+  # }
+  consul_services = {
+    for name, ids in local.consul_service_ids :
+    name => [for id in ids : var.services[id]]
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,53 @@
+#
+# var.services is a required input variable for Consul Terraform Sync
+#
+variable "services" {
+  description = "Consul services monitored by Consul-Terraform-Sync"
+  type = map(
+    object({
+      id        = string
+      name      = string
+      kind      = string
+      address   = string
+      port      = number
+      meta      = map(string)
+      tags      = list(string)
+      namespace = string
+      status    = string
+
+      node                  = string
+      node_id               = string
+      node_address          = string
+      node_datacenter       = string
+      node_tagged_addresses = map(string)
+      node_meta             = map(string)
+
+      cts_user_meta = map(string)
+    })
+  )
+}
+
+#
+# Your module for can include input variables to be used by CTS operators for
+# customizing the module based on their infastructure. CTS supports both
+# optional and required variables.
+#
+variable "address_group_prefix" {
+  description = "Prefix added to each address group name"
+  type        = string
+
+  # Set the default argument to a default value to declare an optional variable.
+  # Omit the default argument for required ariables.
+  default = ""
+
+  # Set the sensitive flag for input variables that contain secret or sensitive
+  # values. When set, Terraform will redact the value from output when Terraform
+  # commands are run.
+  sensitive = false
+}
+
+variable "address_group_tags" {
+  description = "List of tag names to add to each address group for filtering of Consul service IPs"
+  type        = list(string)
+  default     = []
+}
